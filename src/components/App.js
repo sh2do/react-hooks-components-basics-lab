fdc3c2a91c6b539051fa5b96f7f94199ea6786bd
jeff <sddeh5981@gmsil.com>
2025-10-08 10:51:42 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,8 @@ function NavBar() {
   return (
     <nav>
       <a href="#home">I'm a link!</a>
+      <a href="#navbar">I'm a link!</a>
+      <a href="#about">I'm a link!</a>
     </nav>
   );
 }
@@ -16,15 +18,20 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
-
+function About() {
+  return (
+    <div id="about">
+      <h2>About us</h2>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
